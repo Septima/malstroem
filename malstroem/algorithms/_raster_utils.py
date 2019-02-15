f6edec2d32f574ed1468bc92a986d3f7f80c5a61
AsgerPetersen <asgerpetersen@gmail.com>
2019-02-15 11:14:38 +0100

    Disable unicode literals

--- malstroem/algorithms/_raster_utils.py
+++ malstroem/algorithms/_raster_utils.py
@@ -11,7 +11,7 @@
 # You should have received a copy of the GNU General Public License along with this program. If not,
 # see http://www.gnu.org/licenses/.
 # -------------------------------------------------------------------------------------------------
-from __future__ import (absolute_import, division, print_function, unicode_literals)
+from __future__ import (absolute_import, division, print_function) #, unicode_literals)
 from builtins import *
 
 
