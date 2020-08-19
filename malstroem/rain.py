b71e0cec1fa9b2ce3216d2e93cece766e8697202
Asger Skovbo Petersen <asger@macbook-pro.home>
2020-08-19 16:01:13 +0200

    Get model input water volume from precip raster
    #17

--- malstroem/rain.py
+++ malstroem/rain.py
@@ -12,9 +12,11 @@
 # see http://www.gnu.org/licenses/.
 # -------------------------------------------------------------------------------------------------
 from __future__ import (absolute_import, division, print_function) #, unicode_literals)
+from enum import Enum
 from builtins import *
 
 from .network import Network
+from .algorithms import label
 import logging
 
 
@@ -61,12 +63,109 @@ class SimpleVolumeTool(object):
         all_nodes = self.input_nodes.read_geojson_features()
         
         self.logger.info("Calculating rain volumes")
-
         self.logger.info("  {}mm".format(self.rainmm))
         for node in all_nodes:
             props = node["properties"]
             area = float(props['wshed_area'])
             wshed_water_vol = area * self.rainmm * 0.001
+            props[self.output_volume_attribute] = wshed_water_vol  # Water vol from local catchment
+
+        self.logger.info("Writing output")
+        self.output_volumedata.write_geojson_features(all_nodes)
+
+        self.logger.info("Done")
+
+
+class Unit(Enum):
+    mm = 1,
+    l = 2,
+    m3 = 3
+
+class RasterVolumeTool(object):
+    """Calculates water volume for each watershed defined by a raster with cells giving input water per cell
+
+    Parameters
+    ----------
+    input_nodes : vectorreader
+        Nodes
+    input_bluespots : rasterreader
+        Bluespot ids raster
+    input_precip : rasterreader
+        Precipitation raster
+    input_precip_unit: Unit
+        Unit of cell value 
+    output_volumedata : vectorwriter
+        Writes the output volumes data
+    output_volume_attribute: str
+        Name of attribute which calculated water volume is written to
+
+    Attributes
+    ----------
+    input_nodes : list
+        Nodes
+    output_volumedata : vectorwriter
+        Writes the output volumes data
+    output_volume_attribute: str
+        Name of attribute which calculated water volume is written to
+    """
+
+    def __init__(self, input_nodes, input_bluespots, input_precip, input_precip_unit, output_volumedata, output_volume_attribute):
+        self.input_nodes = input_nodes
+        self.output_volumedata = output_volumedata
+        self.output_volume_attribute = str(output_volume_attribute)
+        self.bluespotids = input_bluespots
+        self.precipraster = input_precip
+        self.precipunit = input_precip_unit
+        self.logger = logging.getLogger(__name__)
+
+    def process(self):
+        """Process
+
+        Returns
+        -------
+        None
+        """
+
+        self.logger.info("Reading input data")
+        all_nodes = self.input_nodes.read_geojson_features()
+        
+        # Input rasters
+        transform = self.bluespotids.transform
+        cell_width = abs(transform[1])
+        cell_height = abs(transform[5])
+        cell_area = cell_width * cell_height
+        bluespot_labels_raster = self.bluespotids.read()
+        water_raster = self.precipraster.read()
+        assert bluespot_labels_raster.shape == water_raster.shape, "Rasters must have same extent and resolution"
+
+        self.logger.info("Calculating water volumes")
+        water_stats = label.label_stats(water_raster, bluespot_labels_raster)
+        
+        # Factor to convert from raster cell unit to m3
+        conversion_factor = 1
+        if self.precipunit is Unit.mm:
+            self.logger.info(f"Cell unit is [mm]")
+            conversion_factor = 1e-3 * cell_area    
+        elif self.precipunit is Unit.l:
+            self.logger.info(f"Cell unit is [l]")
+            conversion_factor = 1e-3
+        elif self.precipunit is Unit.m3:
+            self.logger.info(f"Cell unit is [m3]")
+            conversion_factor = 1
+        else:
+            raise NotImplementedError(F"Unit {self.precipunit} is not implemented")
+
+        self.logger.info(f"Using conversion factor {conversion_factor} to go from cell values to m3")
+        water_volumes = water_stats["sum"] * conversion_factor
+
+        max_id = len(water_volumes) - 1
+        for node in all_nodes:
+            props = node["properties"]
+            bsid = props["nodeid"]
+            wshed_water_vol = 0
+            # Do we have a value for this node? (junctions do not have a bluespot id and hence no label)
+            if bsid <= max_id:
+                wshed_water_vol = water_volumes[bsid]
             props[self.output_volume_attribute] = wshed_water_vol  # Water vol from local catchment
 
         self.logger.info("Writing output")
--- tests/test_rain.py
+++ tests/test_rain.py
@@ -1,8 +1,10 @@
-from data.fixtures import nodesreader
-from malstroem.rain import SimpleVolumeTool
-from malstroem.io import VectorWriter
+from data.fixtures import nodesreader, precipraster_float_file, precipraster_byte_file, labeledfile
+from malstroem.rain import SimpleVolumeTool, RasterVolumeTool, Unit
+from malstroem.io import VectorWriter, RasterReader
 from osgeo import osr, ogr
 import json
+import pytest
+import numpy as np
 
 
 def create_utm32_crs():
@@ -24,4 +26,70 @@ def test_evenly_distributed(nodesreader, tmp_path):
         parsed = json.load(f)
     assert all([ x["properties"][attr] > 0 for x in parsed["features"] if x["properties"]["nodetype"] == "pourpoint"]), "One or more pourpoints having 0m3 rain volume"
     assert sum([x["properties"][attr] for x in parsed["features"]]) > 0, "Volumes sum to 0"
+    
+@pytest.mark.parametrize("precipfile", [precipraster_byte_file, precipraster_float_file])
+def test_rasterdefined(nodesreader, tmp_path, precipfile):
+    volumes_file_path = tmp_path / "volumes.geojson"
+    crs = create_utm32_crs()
+    volumes_writer = VectorWriter("geojson", str(volumes_file_path), "", None, ogr.wkbPoint, crs)
+    attr = "inputvol"
+
+    bspotlabels_reader = RasterReader(labeledfile)
+    precip_reader = RasterReader(precipfile)
+
+    unit = Unit.mm
+    tool = RasterVolumeTool(nodesreader, bspotlabels_reader, precip_reader, unit, volumes_writer, attr)
+    tool.process()
+
+    assert volumes_file_path.is_file(), "No file written"
+    with open(volumes_file_path) as f:
+        parsed = json.load(f)
+    for node in parsed["features"]:
+        props = node["properties"]
+        if props["nodetype"] == "pourpoint":
+            nodeid = props["nodeid"]
+            # Some watersheds should not receive water at all as they are outside the event (these are not all the watersheds with 0 water)
+            if nodeid in (52, 48, 96, 27):
+                assert props[attr] == 0, "These watershed should not have any water from the precip raster"
+            elif nodeid in (61, 63, 59):
+                # Some watersheds should receive water (these are not all the watersheds with 0 water)
+                assert props[attr] > 0, "These watershed should have any water from the precip raster"
+        else:
+            # junction
+            assert props[attr] == 0, "Junctions should not receive water in this process"
+
+    assert sum([x["properties"][attr] for x in parsed["features"]]) > 0, "Volumes sum to 0"
+    assert len(nodesreader.read_geojson_features()) == len(parsed["features"]), "Process has dropped features"
+
+
+def test_rasterdefined_unit(nodesreader, tmp_path):
+    crs = create_utm32_crs()  
+    attr = "inputvol"
+
+    bspotlabels_reader = RasterReader(labeledfile)
+    precip_reader = RasterReader(precipraster_float_file)
+
+    def calc(unit):
+        # Returns the total volume in m3
+        volumes_file_path = tmp_path / f"volumes_{unit}.geojson"
+        volumes_writer = VectorWriter("geojson", str(volumes_file_path), "", None, ogr.wkbPoint, crs)
+        tool = RasterVolumeTool(nodesreader, bspotlabels_reader, precip_reader, unit, volumes_writer, attr)
+        tool.process()
+        with open(volumes_file_path) as f:
+            parsed = json.load(f)
+        return sum( [ x["properties"][attr] for x in parsed["features"] ] )
+
+    # This is the sum of all cell values
+    sum_precip_raster = np.sum(precip_reader.read())
+
+    calc_m3 = calc(Unit.m3)
+    assert calc_m3 == sum_precip_raster
+
+    calc_l = calc(Unit.l)
+    assert calc_l == pytest.approx(sum_precip_raster / 1000)
+
+    calc_mm = calc(Unit.mm)
+    cell_area = precip_reader.resolution[0] * precip_reader.resolution[1]
+    assert calc_mm == pytest.approx(sum_precip_raster * 1e-3 * cell_area) # mm to meters, cell area
+
     
