79dcd261e553de739dae774ddf510bcb94d9a0c0
Maja Dall <maja@septima.dk>
2024-09-13 13:40:16 +0200

    Rettet PROJ_LIB til PROJ_DATA

--- pyinstaller/proj_runtime_hook.py
+++ pyinstaller/proj_runtime_hook.py
@@ -4,7 +4,6 @@ from pathlib import Path
 
 # This is a runtime hook which sets the PROJ_LIB env var to point at 
 # the bundled proj data
-proj_lib = os.environ.get("PROJ_LIB")
-if not proj_lib:
-    proj_path = Path(sys._MEIPASS) / "proj_data"
-    os.environ['PROJ_LIB'] = str(proj_path)
+
+proj_path = Path(sys._MEIPASS) / "proj_data"
+os.environ['PROJ_DATA'] = str(proj_path)
