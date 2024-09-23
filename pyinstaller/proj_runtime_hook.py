8d276a1f7479f159cceb3657b9b260472d07b466
AsgerPetersen <asgerpetersen@gmail.com>
2024-09-23 11:56:52 +0200

    Fix PROJ_DATA comment

--- pyinstaller/proj_runtime_hook.py
+++ pyinstaller/proj_runtime_hook.py
@@ -2,7 +2,7 @@ import sys
 import os
 from pathlib import Path
 
-# This is a runtime hook which sets the PROJ_LIB env var to point at 
+# This is a runtime hook which sets the PROJ_DATA env var to point at 
 # the bundled proj data
 
 proj_path = Path(sys._MEIPASS) / "proj_data"
